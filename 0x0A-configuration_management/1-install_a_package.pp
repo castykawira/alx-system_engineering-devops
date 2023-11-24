9d4c0068bab7b5961015288fd4f302d272046abc
castykawira <murithicasty@gmail.com>
2023-11-24 11:06:13 -0800

    question 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Installs puppet-lint
-
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+# Using Puppet, install flask from pip3
+package { 'Werkzeug':
+  ensure   => '2.2.2',
+  provider => 'pip3',
 }

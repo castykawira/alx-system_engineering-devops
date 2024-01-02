2d2953973ffe2efeca3a2d5a07cb8038dc6c87d0
castykawira <murithicasty@gmail.com>
2024-01-02 09:38:30 -0800

    question 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Script to get and display an employee's TODO list progress"""
+"""Accessing a REST API for todo lists of employees"""
 
 import requests
 import sys
 
 
 if __name__ == '__main__':
-    employee_id = sys.argv[1]
-    api_url = "https://jsonplaceholder.typicode.com/users"
-    url = api_url + "/" + employee_id
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
     response = requests.get(url)
     username = response.json().get('username')

7ccb0c17c3b641d15eaa86bd2b2513652040e730
castykawira <murithicasty@gmail.com>
2024-01-18 11:12:53 -0800

    question 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Provides a function to print the titles of the first 10 hot posts
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Print the titles of the first 10 hot posts for a Reddit subreddit
+    """
+    url = 'https://www.reddit.com'
+    header = {
+        'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    }
+    response = requests.get('{}/r/{}/.json?sort={}&limit={}'.format(
+        url, subreddit, 'top', 10),
+        headers=header,
+        allow_redirects=False)
+    if response.status_code == 200:
+        for post in response.json()['data']['children'][0:10]:
+            print(post['data']['title'])
+    else:
+        print(None)
